e628110af9e04c039d775444def3b45fdc245138
Jeron A. Lau <jeron.lau@plopgrizzly.com>
2018-08-30 11:30:33 -0500

    Deprecate this crate, consolidated into ADI.

--- src/lib.rs
+++ src/lib.rs
@@ -5,33 +5,5 @@
 
 //! Render graphics to a computer or phone screen, and get input.  Great for
 //! both video games and apps!
-
-#![warn(missing_docs)]
-#![doc(html_logo_url = "http://plopgrizzly.com/adi_screen/icon.png",
-	html_favicon_url = "http://plopgrizzly.com/adi_screen/icon.ico",
-	html_root_url = "http://plopgrizzly.com/adi_screen/")]
-
-/*mod window;
-mod sprite;
-mod gui;
-mod texture;
-#[doc(hidden)]
-pub mod prelude;*/
-
-/*pub use prelude::{ Transform, Vec3 };
-pub use window::{ Window, Widget };
-pub use sprite::{ Sprite };
-pub use texture::Texture;
-pub use gpu_data::{ Model, ModelBuilder };*/
-
-mod gpu_data;
-
-pub mod prelude;
-
-extern crate awi;
-extern crate aci_png;
-extern crate fonterator;
-
-pub use awi::screen::{Screen, ScreenError, Shape, Gradient, Model, Texture, TexCoords};
-pub use awi::render::{Event, Mat4, Transform};
-pub use awi::render::afi::*;
+//!
+//! NOTICE: This crate has been consolidated into the [ADI](https://crates.io/crates/adi) crate.
